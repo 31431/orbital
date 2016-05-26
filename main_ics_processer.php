ab84a33e67cc50cfb9556dae98f473b93f6dcf47
31431 <kitsada280@gmail.com>
2016-05-27 00:35:28 +0800

    Linking all 4 codes together.

--- main_ics_processer.php
+++ main_ics_processer.php
@@ -53,73 +53,29 @@ function comparison($array1, &$freeTimeArray){
 	}
 }
 //Creating $freeTimeArray where 0 means free
-$dayArray = array(
-         "0600"=>0,
-         "0630"=>0,
-         "0700"=>0,
-         "0730"=>0,
-         "0800"=>0,
-         "0830"=>0,
-         "0900"=>0,
-         "0930"=>0,
-         "1000"=>0,
-         "1030"=>0,
-         "1100"=>0,
-         "1130"=>0,
-         "1200"=>0,
-         "1230"=>0,
-         "1300"=>0,
-         "1330"=>0,
-         "1400"=>0,
-         "1430"=>0,
-         "1500"=>0,
-         "1530"=>0,
-         "1600"=>0,
-         "1630"=>0,
-         "1700"=>0,
-         "1730"=>0,
-         "1800"=>0,
-         "1830"=>0,
-         "1900"=>0,
-         "1930"=>0,
-         "2000"=>0,
-         "2030"=>0,
-         "2100"=>0,
-         "2130"=>0,
-         "2200"=>0,
-         "2230"=>0,
-         "2300"=>0,
-         "2330"=>0);
-   // Array for the weekArray
-$freeTimeArray = array(
-         "Monday" => $dayArray,
-         "Tuesday" => $dayArray,
-         "Wednesday" => $dayArray,
-         "Thursday" => $dayArray,
-         "Friday" => $dayArray,
-         "Saturday" => $dayArray,
-         "Sunday" => $dayArray);
+function doingComparison($userArray, $freeTimeArray){
+
 
 
 //creating 2 objects. 
-
+/*
 $user1 = new user("sample.ics");
 $user1->allfunctionsprocessing();
 $user2 = new user("sample2.ics");
-$user2->allfunctionsprocessing();
+$user2->allfunctionsprocessing();*/
 
 
-comparison($user1->weekArray, $freeTimeArray);
-comparison($user2->weekArray, $freeTimeArray);
+comparison($userArray, $freeTimeArray);
+//comparison($user2->weekArray, $freeTimeArray);
 echo "<br><br>=======Free Time Array=======<br><br>";
 //Function below is from icsToArray.php
-testPrintArray($freeTimeArray);
+/*testPrintArray($freeTimeArray);
 for($i=0;$i<5;$i++){
 	${'value'.$i}=$i;
 }
-echo $user2->name;
+echo $user2->name;*/
 
 
 
-
+}
 ?>
--- upload.php
+++ upload.php
@@ -5,22 +5,24 @@
 </head>
 <body>
 <?php
+require_once("main_ics_processer.php");
 $target_dir = "uploads/";
 // Count # of uploaded files in array
 $total = count($_FILES['fileToUpload']['name']);
+
 
 // Loop through each file
 for($i=0; $i<$total; $i++) {
   //Get the temp file path
   $tmpFilePath = $_FILES['fileToUpload']['tmp_name'][$i];
-
   //Make sure we have a filepath
   if ($tmpFilePath != ""){
     //Setup our new file path
     $temp = explode(".", $_FILES["fileToUpload"]["name"][$i]);
     $date = date_create();
-    $newfilename = date_timestamp_get($date).$_FILES['fileToUpload']['name'][$i] . '.' . end($temp);
+    $newfilename = date_timestamp_get($date).$_FILES['fileToUpload']['name'][$i];
     $newFilePath = "uploads/" . $newfilename;
+    echo $newFilePath."<br>";
 
   }
 }
@@ -80,10 +82,69 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }}
+
+     $j=$i+1;
+     ${'user'.$j}= new user($newFilePath);
+     ${'user'.$j}->allfunctionsprocessing();
 }
 
-include("icstoarray.php");
-print_r($newFilePath);
+//Creating freeTimeArray
+$dayArray = array(
+         "0600"=>0,
+         "0630"=>0,
+         "0700"=>0,
+         "0730"=>0,
+         "0800"=>0,
+         "0830"=>0,
+         "0900"=>0,
+         "0930"=>0,
+         "1000"=>0,
+         "1030"=>0,
+         "1100"=>0,
+         "1130"=>0,
+         "1200"=>0,
+         "1230"=>0,
+         "1300"=>0,
+         "1330"=>0,
+         "1400"=>0,
+         "1430"=>0,
+         "1500"=>0,
+         "1530"=>0,
+         "1600"=>0,
+         "1630"=>0,
+         "1700"=>0,
+         "1730"=>0,
+         "1800"=>0,
+         "1830"=>0,
+         "1900"=>0,
+         "1930"=>0,
+         "2000"=>0,
+         "2030"=>0,
+         "2100"=>0,
+         "2130"=>0,
+         "2200"=>0,
+         "2230"=>0,
+         "2300"=>0,
+         "2330"=>0);
+   // Array for the weekArray
+$freeTimeArray = array(
+         "Monday" => $dayArray,
+         "Tuesday" => $dayArray,
+         "Wednesday" => $dayArray,
+         "Thursday" => $dayArray,
+         "Friday" => $dayArray,
+         "Saturday" => $dayArray,
+         "Sunday" => $dayArray);
+
+
+comparison($user1->weekArray, $freeTimeArray);
+echo "<br><br>=======Free Time Array=======<br><br>";
+testPrintArray($freeTimeArray);
+
+
+
+
+
 ?>
 </body>
 </html>
